7cd99a6f4a10638fe754c122738feae2ac68b584
Victor Stone <assoverteakettle.org@gmail.com>
2012-12-12 13:51:00 +1100

    Initial Commit

---
+++ TimbreGroove/TGViewController.h
@@ -0,0 +1,14 @@
+//
+//  TGViewController.h
+//  TimbreGroove
+//
+//  Created by victor on 12/12/12.
+//  Copyright (c) 2012 Ass Over Tea Kettle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface TGViewController : GLKViewController
+
+@end
---
+++ TimbreGroove/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TimbreGroove
+//
+//  Created by victor on 12/12/12.
+//  Copyright (c) 2012 Ass Over Tea Kettle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TGAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TGAppDelegate class]));
+    }
+}
